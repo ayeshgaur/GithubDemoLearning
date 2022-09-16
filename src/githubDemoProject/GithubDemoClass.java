b277f47202049131a11f1e651d6051bd96162a5f
Vinay <Vinay@Vinay-Lenovo-PC.verizon.net>
2022-09-16 00:15:03 -0400

    My first commit

---
+++ src/githubDemoProject/GithubDemoClass.java
@@ -0,0 +1,15 @@
+package githubDemoProject;
+
+public class GithubDemoClass {
+	
+	public void demoMethod() {
+		System.out.println("first github demo");
+		
+	}
+
+	public void main(String [] args) {
+		 
+		demoMethod();
+	}
+	
+}
